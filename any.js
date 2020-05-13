47fb5336ad2a2763431a4a5233aee7fe6406307a
Kayle Knopp <kayle@qubit.com>
2020-05-13 21:33:49 +0100

    Add .any function

---
+++ any.js
@@ -0,0 +1,24 @@
+var promise = require('./index')
+
+module.exports = function any (values) {
+  function flip (p) {
+    return promise(function (resolve, reject) {
+      promise.resolve(p).then(reject, resolve)
+    })
+  }
+  return flip(promise(function (resolve, reject) {
+    var l
+    var counter = l = values && values.length
+    if (!counter) return reject(values)
+    for (var i = 0; i < l; i++) { loop(values[i], i) }
+    function loop (val, l) {
+      if (val && typeof val.then === 'function') {
+        return flip(val.then(reject, function decrement (val) {
+          values[l] = val
+          --counter || resolve(values)
+        }))
+      }
+      reject(val)
+    }
+  }))
+}
--- extra.js
+++ extra.js
@@ -1,5 +1,6 @@
 var Promise = require('./index')
 Promise.all = require('./all')
+Promise.any = require('./any')
 Promise.race = require('./race')
 Promise.resolve = require('./resolve')
 Promise.reject = require('./reject')
